ec010a1c6d1d9fcf2dda21b0b17e55cd534c9757
SalwaMustafa <salwa2532003@gmail.com>
2025-12-22 22:16:02 +0200

    Add enum classes for database collections, file extensions, and response messages

---
+++ src/enums/DatabaseEnum.py
@@ -0,0 +1,7 @@
+from enum import Enum
+
+class DatabaseEnum(Enum):
+
+    RESEARCH_COLLECTION_NAME = "ResearchPapers"
+    SURVEY_COLLECTION_NAME = "SurveyPapers"
+    
---
+++ src/enums/__init__.py
@@ -0,0 +1,3 @@
+from .DatabaseEnum import DatabaseEnum
+from .FileExtensionEnum import FileExtensionEnum
+from .ResponseEnum import ResponseEnum

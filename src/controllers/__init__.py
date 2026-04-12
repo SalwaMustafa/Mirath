8e06f913b00b57c5ed013c540aba93bde6185074
Rawan Elmoafy <rawanelmoafy@gmail.com>
2026-04-12 03:16:29 +0200

    Add ChatController for managing chat metadata and history

--- src/controllers/__init__.py
+++ src/controllers/__init__.py
@@ -2,3 +2,4 @@ from .UploadDataController import UploadDataController
 from .PapersController import PapersController
 from .NLPController import NLPController
 from .BaseController import BaseController
+from .ChatController import ChatController

8f0af5d5802e8e69279a8cac397ceb279344b553
Rawan Elmoafy <rawanelmoafy@gmail.com>
2026-04-21 01:36:26 +0200

    Add chat functionality with audio and image processing capabilities

--- src/routes/__init__.py
+++ src/routes/__init__.py
@@ -1,5 +1,5 @@
 from .Data import data_router
 from .Healthy_Check import healthy_check_router
 from .Search import search_router
-from .Socket_Events import init_socket, socket_app
+from .Chat import chat_router
 from .AI_Services import ai_services_router
